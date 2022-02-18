5be090380c062b4b3fe59c5e78c8624ce4f044a3
chop-suey <xod_2@hotmail.com>
2022-02-18 14:55:53 +0100

    Group splits by day and add day summary component

---
+++ src/components/DaySummary.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+import type { Datetime } from "../model/datetime";
+
+export let datetime: Datetime;
+
+$: date = datetime?.getDateText();
+
+</script>
+
+<style>
+    div {
+        background-color: gray;
+    }
+</style>
+
+<div>{#if !!date}{ date }{/if}</div>
--- src/components/TimeSplits.svelte
+++ src/components/TimeSplits.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
+import { action_destroyer } from "svelte/internal";
+
 import { derived } from "svelte/store";
+import type { Datetime } from "../model/datetime";
 import type { Timesplit } from "../model/timesplit";
 
 import { timeSplitService } from "../service/time-split-service";
+import DaySummary from "./DaySummary.svelte";
 import TimeSplit from "./TimeSplit.svelte";
 
 const splits = derived(timeSplitService.getSplits(), matchAdjacentSplits);
+const groupedSplits = derived(splits, groupSplitsByDay);
 
 // TODO move to split service
 function matchAdjacentSplits(splits: Timesplit[]): Timesplit[] {
@@ -13,6 +18,19 @@ function matchAdjacentSplits(splits: Timesplit[]): Timesplit[] {
         const next = index > 0 ? arr[index - 1] : null;
         return [ (!next ? split : split.withEnd(next.start)), ...acc ];
     }, []);
+}
+
+function groupSplitsByDay(splits: Timesplit[]): { date: Datetime, splits: Timesplit[]}[] {
+    const valueIterator = splits.reduce((acc, curr) => {
+        const key = curr.start.getDateText();
+        if (!acc.has(key)) {
+            acc.set(key, []);
+        }
+        acc.get(key).push(curr);
+        return acc;
+    }, new Map<string, Timesplit[]>()).values();
+    return [ ...valueIterator ]
+        .map(splits => ({ date: splits[0].start.withTime(0, 0), splits }));
 }
 </script>
 
@@ -22,8 +40,12 @@ function matchAdjacentSplits(splits: Timesplit[]): Timesplit[] {
     }
 </style>
 
-<ul class="splits">
-    {#each $splits as split}
-    <li><TimeSplit split={split}></TimeSplit></li>
+{#each $groupedSplits as {date, splits}}
+    <DaySummary datetime={date}></DaySummary>
+    <ul class="splits">
+        {#each splits as split}
+        <li><TimeSplit split={split}></TimeSplit></li>
     {/each}
 </ul>
+{/each}
+
